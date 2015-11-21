97a547d00bba6e34038d3de67b8e1d2bd0c15e43
sobird <crossyou2009@gmail.com>
2015-11-21 11:43:30 +0800

    vb syntax hight

---
+++ Vb.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * vb syntax hight
+ * 
+ * @author  Yang,junlong at 2015-11-21 11:40:30 build.
+ * @version $Id$
+ */
+
+class Vb extends SyntaxHighLighter{
+	
+	public function __construct () {
+		$keywords =	'AddHandler AddressOf AndAlso Alias And Ansi As Assembly Auto 
+					Boolean ByRef Byte ByVal Call Case Catch CBool CByte CChar CDate 
+					CDec CDbl Char CInt Class CLng CObj Const CShort CSng CStr CType 
+					Date Decimal Declare Default Delegate Dim DirectCast Do Double Each 
+					Else ElseIf End Enum Erase Error Event Exit False Finally For Friend 
+					Function Get GetType GoSub GoTo Handles If Implements Imports In 
+					Inherits Integer Interface Is Let Lib Like Long Loop Me Mod Module 
+					MustInherit MustOverride MyBase MyClass Namespace New Next Not Nothing 
+					NotInheritable NotOverridable Object On Option Optional Or OrElse 
+					Overloads Overridable Overrides ParamArray Preserve Private Property 
+					Protected Public RaiseEvent ReadOnly ReDim REM RemoveHandler Resume 
+					Return Select Set Shadows Shared Short Single Static Step Stop String 
+					Structure Sub SyncLock Then Throw To True Try TypeOf Unicode Until 
+					Variant When While With WithEvents WriteOnly Xor';
+
+		$this->regexList = array(
+			array(
+				'regex' => "/'.*$/m",
+				'css' => 'comment'
+			),
+			array(
+				'regex' => '/^\\s*#.*/m',
+				'css' => 'preprocessor'
+			),
+			array(
+				'regex' => $this->genRegex($keywords),
+				'css' => 'keyword'
+			)
+		);
+	}
+}
